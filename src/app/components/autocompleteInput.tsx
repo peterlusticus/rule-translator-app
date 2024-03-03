4d3e91dcb94c5cecc8c718a0d977365ab1c326af
Richard <peter.lusticus@gmail.com>
2024-03-03 18:42:25 +0100

    ui ready 2

--- src/app/components/autocompleteInput.tsx
+++ src/app/components/autocompleteInput.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Combobox } from '@headlessui/react'
 import { fields } from '../data/data'
+import { CalculatorIcon, ChevronUpDownIcon, ListBulletIcon, PencilIcon } from '@heroicons/react/20/solid'
 
 function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
@@ -9,6 +10,10 @@ function classNames(...classes: string[]) {
 export function AutocompleteInput(porps: any) {
     const [selectedField, setSelectedField] = useState('')
     const [query, setQuery] = useState('')
+    const [selectedDatatype, setSelectedDatatype] = useState("string")
+
+    console.log(selectedDatatype)
+
 
     const filteredFileds =
         query === ''
@@ -17,10 +22,26 @@ export function AutocompleteInput(porps: any) {
                 return field.toLowerCase().includes(query.toLowerCase())
             })
 
+    useEffect(() => {
+        if (filteredFileds.indexOf(selectedField) == 0) {
+            setSelectedDatatype("list")
+        } else if (isNaN(Number(selectedField))) {
+            setSelectedDatatype("string")
+        } else{
+            setSelectedDatatype("number")
+        }
+    }, [selectedField]);
+
+
     return (
         <Combobox value={selectedField} onChange={setSelectedField} nullable>
             <div className="relative w-full">
+
+
                 <Combobox.Input onChange={(event) => setQuery(event.target.value)} displayValue={(person) => selectedField} className="relative w-full rounded-none border border-gray-300 bg-white text-left outline-none form-dropdown form-input cursor-text" />
+                {selectedDatatype == "list" && <div className="pointer-events-none absolute inset-y-0 right-0 ml-3 mt-2.5 items-center pr-2"> <ListBulletIcon className="h-5 w-5 text-gray-400" aria-hidden="true" /> </div>}
+                {selectedDatatype == "string" && <div className="pointer-events-none absolute inset-y-0 right-0 ml-3 mt-2.5 items-center pr-2"> <PencilIcon className="h-5 w-5 text-gray-400" aria-hidden="true" /> </div>}
+                {selectedDatatype == "number" && <div className="pointer-events-none absolute inset-y-0 right-0 ml-3 mt-2.5 items-center pr-2"> <CalculatorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" /> </div>}
                 <Combobox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-none bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                     {query.length > 0 && (
                         <Combobox.Option value={query} hidden />
