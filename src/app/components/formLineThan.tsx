0cfc120c78e0597c551eee6e86ec5470f7ec56bd
Richard <peter.lusticus@gmail.com>
2024-03-04 15:18:16 +0100

    n

--- src/app/components/formLineThan.tsx
+++ src/app/components/formLineThan.tsx
@@ -18,8 +18,8 @@ export function FormLineThan(props: any) {
     <div>
       <FormLine>
         <div className="w-2/5 mr-4"><Dropdown items={fields} prop={"field_" + prop} order={false} /></div>
-        <div className="w-1/5 mr-4"><Dropdown items={visibility} prop={"field_" + prop} order={false} /></div>
-        <div className="w-1/5 mr-4"><Dropdown items={required} prop={"field_" + prop} order={false} /></div>
+        <div className="w-1/5 mr-4"><Dropdown items={visibility} prop={"visibility_" + prop} order={false} /></div>
+        <div className="w-1/5 mr-4"><Dropdown items={required} prop={"required_" + prop} order={false} /></div>
         {prop !== 1 && <button type="button" className="mb-2 w-11 rounded-none bg-red-600 hover:bg-red-500 text-xl text-center text-base text-white " onClick={handleDel}> 🗑</button>}
         {prop == 1 && <button type="button" className="mb-2 w-11 rounded-none bg-gray-300 hover:bg-gray-200 text-xl text-center text-base text-white " onClick={handleDel}>🗑</button>}
         <button type="button" className="mb-2 ml-3 mr-2 w-11 rounded-none bg-green-600 hover:bg-green-500 text-xl text-center text-base text-white " onClick={handleAdd}>+</button>
